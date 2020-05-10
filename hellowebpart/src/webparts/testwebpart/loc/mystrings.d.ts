b45e483113c9227d52476c30e108a0c733b8c4de
gdnaidu <gdnaidu@432gmail.com>
2020-05-10 13:05:22 +0530

    basicversion

---
+++ hellowebpart/src/webparts/testwebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITestwebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TestwebpartWebPartStrings' {
+  const strings: ITestwebpartWebPartStrings;
+  export = strings;
+}
